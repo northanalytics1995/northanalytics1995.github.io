b6462f110678b40c2558b8f3af35d20d7bad1409
Dylan <dylan0226l@gmail.com>
2024-09-14 14:37:14 -0400

    Fix warnings.

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./footer.styles.scss";
 import { Link } from "react-router-dom";
 // import reactLogo from "../../img/react-logo-3.png";
-import reactLogo from "../../img/react-logo-3.png";
 import preval from "preval.macro";
 // import VisibilitySensor from "react-visibility-sensor";
 
